4a270200841a3ab7b243e7deba3170ea9e56e723
RUDDHI SWASTIK NAYAK <eee.25beed15@silicon.ac.in>
2026-06-08 21:02:58 +0530

    Update pair_with_difference.py

--- optimization_practice/pair_with_difference.py
+++ optimization_practice/pair_with_difference.py
@@ -79,3 +79,37 @@ overall O(n) algorithm.
 =================================================
 
 """
+def has_pair_brute(nums, k):
+    n = len(nums)
+
+    for i in range(n):
+        for j in range(i + 1, n):
+            if abs(nums[i] - nums[j]) == k:
+                return True
+
+    return False
+
+
+def has_pair_fast(nums, k):
+    num_set = set(nums)
+
+    for x in nums:
+        if (x + k) in num_set or (x - k) in num_set:
+            return True
+
+    return False
+
+
+# Input Example 1
+nums = [1, 5, 3, 4, 2]
+k = 3
+
+print("Brute Force:", has_pair_brute(nums, k))
+print("Optimized:", has_pair_fast(nums, k))
+
+# Input Example 2
+nums = [8, 12, 16, 4, 0, 20]
+k = 9
+
+print("Brute Force:", has_pair_brute(nums, k))
+print("Optimized:", has_pair_fast(nums, k))
